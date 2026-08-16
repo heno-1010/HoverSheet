9642b5e1beedd9ed7a6d61ee8b7456921015f7f8
heno-1010 <heno-1010@users.noreply.github.com>
2026-08-16 17:01:08 +0900

    Initial commit

---
+++ HoverSheet/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HoverSheet.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ HoverSheet/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace HoverSheet.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
