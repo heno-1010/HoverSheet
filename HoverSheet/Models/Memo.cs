73dc58bbbffc832353e64fc4637e642036563f90
heno-1010 <heno-1010@users.noreply.github.com>
2026-08-17 17:33:58 +0900

    Implement MemoCollection CRUD operations

---
+++ HoverSheet/Models/Memo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HoverSheet.Models
+{
+    internal class Memo
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ HoverSheet/Models/MemoCollection.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoverSheet.Models
+{
+    internal class MemoCollection
+    {
+        public List<Memo> Memos { get; set; } = new List<Memo>();
+
+        public void AddMemo(string content)
+        {
+            var memo = new Memo
+            {
+                Id = Guid.NewGuid(),
+                Content = content
+            };
+            Memos.Add(memo);
+        }
+        public void RemoveMemo(Guid id)
+        {
+            var memo = Memos.Find(m => m.Id == id);
+            if (memo != null)
+            {
+                Memos.Remove(memo);
+            }
+        }
+        public Memo? GetMemo(Guid id)
+        {
+            return Memos.Find(m => m.Id == id);
+        }
+        public void UpdateMemo(Guid id, string newContent)
+        {
+            var memo = Memos.Find(m => m.Id == id);
+            if (memo != null)
+            {
+                memo.Content = newContent;
+            }
+        }   
+    }
+}
